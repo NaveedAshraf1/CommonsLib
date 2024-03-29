64ec6cf215412e808e8ee8697f2622e04045c939
Naveed <naveedashraf3455@gmail.com>
2024-03-29 08:10:57 +0500

    device info deleted

--- lycommons/build.gradle.kts
+++ lycommons/build.gradle.kts
@@ -108,7 +108,7 @@ dependencies {
 
 
     // work manager
-    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("devandroidx.work:work-runtime-ktx:2.7.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
